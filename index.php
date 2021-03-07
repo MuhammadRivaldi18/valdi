5b2d71094dd6ef513ca8695075a88f30084936ba
MuhammadRivaldi18 <80207761+MuhammadRivaldi18@users.noreply.github.com>
2021-03-07 23:19:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="Viewport" content="width=device-width, initial-scale=1">
+	<title>My Website</title>
+	<link rel="stylesheet" type="text/css" href="halaman1.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+		<div class="main">
+			<hi><a href="index.html">My Website </a></hi>
+			<ul>
+				<li class="active"><a href="My Project.html">My Project</a></li>
+				<li><a href="About Me.html">About Me</a></li>
+				<li><a href="Ask Me.html">Ask me</a></li>
+			</ul>
+		</div>	
+		</div>
+	</header>
+</body>
+
+	<!-- benner -->
+	<section class="banner">
+		<h2>My Project</h2>
+	
+	<style type="text/css">
+		.tabel {
+			font-family:sans-serif;
+			color: #444;
+			border-collapse: collapse;
+			width: 80%;
+			border: 1px solid;
+			margin-top: 70px;
+		}
+	</style>
+
+<body>
+	<table class="tabel">
+		<tr>
+			<th>No</th>
+			<th>PROJECT</th>
+			<th>AS</th>
+			<th>DESCRIPTION</th>
+		</tr>
+
+		<tr>
+			<th>1</th>
+			<th>Music Recomendation based on Face Expression Detection</th>
+			<th>Data Analist</th>
+			<th>Devolop exiting music applicatiion by ading features to recommend music playlist based on facial expression by using facial detection</th>
+		</tr>
+
+		<tr>
+			<th>2</th>
+			<th>Helpdesk</th>
+			<th>Fronted</th>
+			<th>It is a website used by the helpdisk to record customer compline processes.</th>
+		</tr>
+
+		<tr>
+			<th>3</th>
+			<th>Little Cop</th>
+			<th>Game Dev</th>
+			<th>In this research, a game called "Little Cop" was made, where this game will a pulic media in knowing the importance of traffic rules and what sanctions will be abtained when violating one or more of the exaiting regulations.</th>
+		</tr>
+</section>
+</table>
+</body>
+</section>
+
+
+<body>
+	<header>
+	<!--  medsos -->
+<div class="medsos">
+	<div class="link">
+	<ul>
+		<li><a href="#">Facebook</a></li>
+		<li><a href="https://instagram.com/rivaldi18_?igshid=1hp404axyln8y">Instagram</a></li>
+		<li><a href="#">Github</a></li>
+	</ul>
+</div>
+</div>
+</header>
+</body>
+
+<!-- about -->
+<body>
+	<header>
+		<div class="container">
+		<div class="main">
+			<hi><a href="index.html">My Website </a></hi>
+			<ul>
+				<li class="active"><a href="My Project.html">My Project</a></li>
+				<li><a href="About Me.html">About Me</a></li>
+				<li><a href="Ask Me.html">Ask me</a></li>
+			</ul>
+		</div>	
+		</div>
+	</header>
+</body>
+
+<section class="hai">
+		<h2 style="padding: 20px; color: #1E90FF;">About Me</h2>
+		<div class="haihai">
+			<img src="img/pict.jpeg" width='200px'height='210px'align="left"style="padding-right: 10px; padding-bottom:150px;">
+		<h3>Hai! Saya Muhammad Rivaldi</h3>
+		<h4 style="padding: 50px; text-alight: center;">Saya Mahasiswa Universitas YARSI semester 4, saya mengambil Jurusan Teknik Informatika, saat ini saya sudah banyak belajar tentang bahasa pemrograman.</h4>
+	
+	<h3 style="padding: 0px; text-alight: center;">Skills</h3>
+	<h4>- ms. world</h4>
+	<h4>- ms. point</h4>
+	<h4>- java</h4>
+	<h4>- html</h4>
+</div>
+</section>
+
+<style type="text/css">
+		.tabel {
+			font-family:sans-serif;
+			color: #444;
+			border-collapse: collapse;
+			width: 80%;
+			border: 1px solid;
+			margin-top: 70px;
+		}
+	</style>
+
+<body>
+	
+</section>
+</table>
+</body>
+</section>
+
+
+<body>
+	<header>
+	<!--  medsos -->
+<div class="medsos">
+	<div class="link">
+	<ul>
+		<li><a href="#">Facebook</a></li>
+		<li><a href="https://instagram.com/rivaldi18_?igshid=1hp404axyln8y">Instagram</a></li>
+		<li><a href="#">Github</a></li>
+	</ul>
+</div>
+</div>
+</header>
+</body>
+
+
+
+
+
+
+
+<!-- Ask -->
+<body>
+	<header>
+		<div class="container">
+		<div class="main">
+			<hi><a href="index.html">My Website </a></hi>
+			<ul>
+				<li class="active"><a href="My Project.html">My Project</a></li>
+				<li><a href="About Me.html">About Me</a></li>
+				<li><a href="Ask Me.html">Ask me</a></li>
+			</ul>
+		</div>	
+		</div>
+	</header>
+</body>
+
+<section class="ask">
+		<h2>Ask Me!</h2>
+		<form style= "padding-left:20px;">
+		<h3 style="color: black"; >Name</h3>
+		<input type="text" name="Nama Depan" placeholder=""Enter Email>
+		<h3 style="color: black";>Email Address</h3>
+		<input type="text" name="Nama Depan" placeholder=""Enter Email>
+		<h5 style="color: black";>we'll never share your email with anyone else</h5>
+		<h3 style="color: black";>Question</h3>
+		<textarea name="message" rows="4"cols="100" placeholder=""></textarea>
+		<button type="Send" value="Send" style="float: right; color: white; background-color: #1E90FF; padding: 5px">Send</button>
+
+			
+
+		
+
+
+</section>
+
+<style type="text/css">
+		.tabel {
+			font-family:sans-serif;
+			color: #444;
+			border-collapse: collapse;
+			width: 80%;
+			border: 1px solid;
+			margin-top: 70px;
+		}
+	</style>
+
+<body>
+	
+</section>
+</table>
+</body>
+</section>
+
+
+<body>
+	<header>
+	<!--  medsos -->
+<div class="medsos">
+	<div class="link">
+	<ul>
+		<li><a href="#">Facebook</a></li>
+		<li><a href="https://instagram.com/rivaldi18_?igshid=1hp404axyln8y">Instagram</a></li>
+		<li><a href="#">Github</a></li>
+	</ul>
+</div>
+</div>
+</header>
+</body>
+</html> 
+
+
+
